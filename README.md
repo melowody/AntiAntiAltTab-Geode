52b281b435825783185b5674c28c75066f4cb130
Melody <melodydevofficial@gmail.com>
2023-09-10 21:03:26 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AntiAntiAltTab
+
+This is where she makes a mod.
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/AppDelegate.hpp>
+using namespace geode::prelude;
+class $modify(AppDelegate) {
+	void applicationDidEnterBackground() {
+		return;
+	}
+};
